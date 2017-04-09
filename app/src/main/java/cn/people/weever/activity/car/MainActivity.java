3c14108fd95c5b95b0de0df8e73e227e6e227f74
ztcao <ztcao@3ti.us>
2017-04-09 20:04:12 +0800

    定位

--- app/src/main/java/cn/people/weever/activity/car/MainActivity.java
+++ app/src/main/java/cn/people/weever/activity/car/MainActivity.java
@@ -1,15 +1,16 @@
 package cn.people.weever.activity.car;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -17,7 +18,6 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -26,35 +26,70 @@ import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.people.weever.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final Intent newIntent(Context packageContext){
-        Intent intent = new Intent(packageContext , MainActivity.class) ;
-        return intent ;
-    }
+    private static final int accuracyCircleFillColor = 0xAAFFFF88;
+    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
+    public MyLocationListener myListener = new MyLocationListener();
 
     // 定位相关
     LocationClient mLocClient;
-    public MyLocationListenner myListener = new MyLocationListenner();
-    private LocationMode mCurrentMode;
     BitmapDescriptor mCurrentMarker;
-    private static final int accuracyCircleFillColor = 0xAAFFFF88;
-    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
-
     MapView mMapView;
     BaiduMap mBaiduMap;
-
-    // UI相关
-    OnCheckedChangeListener radioButtonListener;
-    Button requestLocButton;
     boolean isFirstLoc = true; // 是否首次定位
+    //UI相关
+    @BindView(R.id.txtAllTime)
+    TextView mTxtAllTime;
+    @BindView(R.id.txtAllWaitTime)
+    TextView mTxtAllWaitTime;
+    @BindView(R.id.txtAllWaitAmount)
+    TextView mTxtAllWaitAmount;
+    @BindView(R.id.ll_top)
+    LinearLayout mLlTop;
+    @BindView(R.id.radioBtnHalfDayUse)
+    RadioButton mRadioBtnHalfDayUse;
+    @BindView(R.id.radioBtnDayUse)
+    RadioButton mRadioBtnDayUse;
+    @BindView(R.id.radioBtnTransfer)
+    RadioButton mRadioBtnTransfer;
+    @BindView(R.id.radioGroup)
+    RadioGroup mRadioGroup;
+    @BindView(R.id.btnStart)
+    Button mBtnStart;
+    @BindView(R.id.btnWait)
+    Button mBtnWait;
+    @BindView(R.id.btnReturn)
+    Button mBtnReturn;
+    @BindView(R.id.btnRestart)
+    Button mBtnRestart;
+    @BindView(R.id.btnCompute)
+    Button mBtnCompute;
+    @BindView(R.id.ll_bottom)
+    LinearLayout mLlBottom;
+    @BindView(R.id.txtAllDistance)
+    TextView mTxtAllDistance;
+    @BindView(R.id.edtSrc)
+    EditText mEdtSrc;
+    @BindView(R.id.edtDest)
+    EditText mEdtDest;
+    private LocationMode mCurrentMode;
+
+    public static final Intent newIntent(Context packageContext) {
+        Intent intent = new Intent(packageContext, MainActivity.class);
+        return intent;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+        ButterKnife.bind(this);
         mCurrentMode = LocationMode.NORMAL;
 
         // 修改为自定义marker
@@ -77,40 +112,13 @@ public class MainActivity extends AppCompatActivity {
         option.setOpenGps(true); // 打开gps
         option.setCoorType("bd09ll"); // 设置坐标类型
         option.setScanSpan(1000);
+        option.setIsNeedAddress(true);
         mBaiduMap
                 .setMyLocationConfigeration(new MyLocationConfiguration(
                         mCurrentMode, true, null,
                         accuracyCircleFillColor, accuracyCircleStrokeColor));
         mLocClient.setLocOption(option);
         mLocClient.start();
-    }
-
-    /**
-     * 定位SDK监听函数
-     */
-    public class MyLocationListenner implements BDLocationListener {
-
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            // map view 销毁后不在处理新接收的位置
-            if (location == null || location.getLocType() == BDLocation.TypeServerError || mMapView == null ) {
-                return;
-            }
-            MyLocationData locData = new MyLocationData.Builder()
-                    .accuracy(location.getRadius())
-                    // 此处设置开发者获取到的方向信息，顺时针0-360
-                    .direction(100).latitude(location.getLatitude())
-                    .longitude(location.getLongitude()).build();
-            mBaiduMap.setMyLocationData(locData);
-            if (isFirstLoc) {
-                isFirstLoc = false;
-                LatLng ll = new LatLng(location.getLatitude(),
-                        location.getLongitude());
-                MapStatus.Builder builder = new MapStatus.Builder();
-                builder.target(ll).zoom(18.0f);
-                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
-            }
-        }
     }
 
     @Override
@@ -134,5 +142,63 @@ public class MainActivity extends AppCompatActivity {
         mMapView.onDestroy();
         mMapView = null;
         super.onDestroy();
+    }
+
+    @OnClick({R.id.btnStart, R.id.btnWait, R.id.btnReturn, R.id.btnRestart, R.id.btnCompute})
+    public void onViewClickedOperate(View view) {
+        switch (view.getId()) {
+            case R.id.btnStart:
+                mLlTop.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btnWait:
+                break;
+            case R.id.btnReturn:
+                break;
+            case R.id.btnRestart:
+                break;
+            case R.id.btnCompute:
+                break;
+        }
+    }
+
+    @OnClick({R.id.edtSrc, R.id.edtDest})
+    public void onViewClickedAddress(View view) {
+        switch (view.getId()) {
+            case R.id.edtSrc:
+                break;
+            case R.id.edtDest:
+                break;
+        }
+    }
+
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || location.getLocType() == BDLocation.TypeServerError || mMapView == null) {
+                return;
+            }
+            String street = location.getStreet();
+            mEdtSrc.setText(street) ;
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
     }
 }
