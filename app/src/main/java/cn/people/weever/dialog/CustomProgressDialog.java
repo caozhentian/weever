93f43571cafd8f3e4e04033a46da4157f3fec1b9
ztcao <ztcao@3ti.us>
2017-05-22 17:28:36 +0800

    M

---
+++ app/src/main/java/cn/people/weever/dialog/CustomProgressDialog.java
@@ -0,0 +1,26 @@
+package cn.com.metro.dialog;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import cn.com.metro.R;
+
+public class CustomProgressDialog extends Dialog {
+
+
+	public CustomProgressDialog(Context context, int themeResId) {
+		super(context, themeResId);
+		//加载布局文件
+		LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view=inflater.inflate(R.layout.progressdialog_layout, null);
+		//dialog添加视图
+		setContentView(view);
+		setCanceledOnTouchOutside(false) ;
+	}
+	public void cancel(){
+		dismiss();
+	}
+}
