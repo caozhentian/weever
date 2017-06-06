86df798d75cdc169bba9b5ced7d4812c156dadbe
ztcao <ztcao@3ti.us>
2017-06-06 22:25:05 +0800

    M

--- app/src/main/java/cn/people/weever/activity/LogoActivity.java
+++ app/src/main/java/cn/people/weever/activity/LogoActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import cn.people.weever.R;
 import cn.people.weever.activity.login.LoginActivity;
+import cn.people.weever.application.WeeverApplication;
 
 public class LogoActivity extends AppCompatActivity {
 
@@ -12,7 +13,12 @@ public class LogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_logo);
-        startActivity(LoginActivity.newIntent(this));
+        if(WeeverApplication.getCurUser() == null) { //需要登录
+            startActivity(LoginActivity.newIntent(this));
+        }
+        else{ //自动登录
+            startActivity(HomeActivity.newIntent(this));
+        }
         finish();
     }
 }
