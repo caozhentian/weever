31299febd4f00f460183bc81a8a37e84c240ab39
ztcao <ztcao@3ti.us>
2017-08-22 15:01:06 +0800

    M

--- app/src/main/java/cn/people/weever/model/Company.java
+++ app/src/main/java/cn/people/weever/model/Company.java
@@ -1,4 +1,4 @@
-package cn.people.weever.model;
+﻿package cn.people.weever.model;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -14,8 +14,17 @@ public class Company implements Serializable{
     @SerializedName("companyNum")
     protected String mCompanyNum  ;
 
+    //会员名称
     @SerializedName("companyName")
     protected String mCompanyName   ;
+
+    //会员类型 普通会员 三星等
+    @SerializedName("type")
+    protected int mType   ;
+
+    //90%
+    @SerializedName("percent")
+    protected String mPercent   ;
 
     public String getCompanyNum() {
         return mCompanyNum;
