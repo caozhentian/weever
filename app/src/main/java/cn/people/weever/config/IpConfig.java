c980f9907052591aa8eac55f210808d628216028
ztcao <ztcao@3ti.us>
2017-09-26 18:07:07 +0800

    M

--- app/src/main/java/cn/people/weever/config/IpConfig.java
+++ app/src/main/java/cn/people/weever/config/IpConfig.java
@@ -7,6 +7,6 @@ public class IpConfig {
 
 	//public final static String BASIC_URL = "http://show.tv.com:8016/";
 	//public final static String BASIC_URL = "http://211.149.188.205:8085/api/";
-	public final static String BASIC_URL = "http://www.xazhihe.cn:8085/";
+	public final static String BASIC_URL = "http://www.xazhihe.cn:8084/";
 
 }
